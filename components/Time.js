51d632049b017f0a497e3d8de9290187b50ed578
Hawad Ahmad <hawadjan.1@gmail.com>
2022-11-03 23:09:23 -0400

    component that shows montreal time

---
+++ components/Time.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import date from 'date-and-time';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
+export default function Time() {
+    const [time, setTime] = useState('');
+
+	useEffect(() => {
+		setTimeout(() => {
+			const now = new Date();
+
+			setTime(date.format(now, 'ddd, MMM DD YYYY h:mm:ss A'));
+		}, 1000)
+	});
+
+    return (
+        <>
+            <FontAwesomeIcon icon={faClock} size={"lg"} />
+            {time}
+        </>
+    );
+}
