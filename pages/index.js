b52c6ffdc7153dff35e009142c84dc05bb5a563f
Hawad Ahmad <hawadjan.1@gmail.com>
2024-09-03 16:10:22 -0400

    use rel noreferrer

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({index}) {
 				<br />
 				<div className='animate__animated animate__fadeInUp animate__faster'>Currently taking
 					<a
-						rel='noopener'
+						rel='noreferrer'
 						target='_blank'
 						className='nav-item underline'
 						style={{ marginLeft: '7px' }}
